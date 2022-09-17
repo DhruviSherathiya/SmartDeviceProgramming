fa422196323c188b7dc208de2c4b9b96a053092f
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-09-17 11:46:42 +0530

    Lab 8 Completed

--- Lab8/lib/code6.dart
+++ Lab8/lib/code6.dart
@@ -10,11 +10,46 @@ class SixthClass extends StatefulWidget {
 
 class _SixthClassState extends State<SixthClass> {
 
+  List<String> quts = [
+    'Talk is cheap, Show me the code',
+    'Real programmers count from 0',
+    'If the code works, Do not touch it'
+  ];
+
   List<Quote> quotes = [
     Quote(text: 'Talk is cheap, Show me the code', author: 'Author 1'),
     Quote(text: 'Real programmers count from 0', author: 'Author 2'),
     Quote(text: 'If the code works, Do not touch it', author: 'Author 3'),
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xffEE6983),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+                color: Color(0xff850E35),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +62,10 @@ class _SixthClassState extends State<SixthClass> {
         elevation: 0.0,
       ),
       body: Column(
-        children: quotes.map ((quote) => Text('${quote.text} - ${quote.author}')).toList() ,
+        //children: quts.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
 }
+
