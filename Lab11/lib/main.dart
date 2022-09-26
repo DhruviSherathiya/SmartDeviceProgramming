a9c24cbb2aff97683bb1397f7987883c82bdd096
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-09-26 23:20:00 +0530

    Lab 11 PDF added

--- Lab11/lib/main.dart
+++ Lab11/lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:lab11/loading.dart';
 import 'package:lab11/home.dart';
-import 'package:lab11/spinkit.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => Spinner(),
-      //'/': (context) => Loading(),
+      '/': (context) => Loading(),
       '/home': (context) => Home(),
     },
   ));
